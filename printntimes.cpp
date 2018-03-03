3c9f9628826a12d742138207b28934df9f9fdc1c
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-03-03 17:38:40 +0530

    Create printntimes.cpp

---
+++ printntimes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n,i;
+  string s;
+  cout<<"Enter the string   ";
+  cin>>s;
+  cout<<"Enter n=";
+  cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cout<<s<<endl;
+    }
+    return 0;
+}
